2d5c998675752e8b762920a55f39c85c5badb302
kmrajibfaysal <kmrajibfaysal@gmail.com>
2024-08-19 16:19:32 +0600

    added new class practice codes

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('working');
+class Color {
+  constructor(r, g, b) {
+    (this.r = r), (this.g = g), (this.b = b);
+  }
+  rgb() {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  testing() {
+    alert('Test Completed!');
+  }
+}
+
+const c1 = new Color(211, 244, 243);
+// console.log(c1);
